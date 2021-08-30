916e1446d00d91b50ec76ba21e3ea207c8b82e0b
Nikita Baliassniy <61225983+nikita-baliassniy@users.noreply.github.com>
2021-08-30 18:08:43 +0300

    lesson2 - flying to the touch point

--- core/src/ru/geekbrains/StarGame.java
+++ core/src/ru/geekbrains/StarGame.java
@@ -1,36 +1,12 @@
 package ru.geekbrains;
 
-import com.badlogic.gdx.ApplicationAdapter;
-import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.badlogic.gdx.graphics.g2d.TextureRegion;
-import com.badlogic.gdx.utils.ScreenUtils;
+import com.badlogic.gdx.Game;
 
-public class StarGame extends ApplicationAdapter {
-	private SpriteBatch batch;
-	private Texture img;
-	private TextureRegion region;
+import ru.geekbrains.screen.MenuScreen;
 
-	private float x;
-	private float y;
-	
+public class StarGame extends Game {
 	@Override
 	public void create () {
-		batch = new SpriteBatch();
-		img = new Texture("wall.jpg");
-	}
-
-	@Override
-	public void render () {
-		ScreenUtils.clear(0.5f, 0.67f, 0.63f, 1f);
-		batch.begin();
-		batch.draw(img, 0, 0, 1920, 1080);
-		batch.end();
-	}
-	
-	@Override
-	public void dispose () {
-		batch.dispose();
-		img.dispose();
+		setScreen(new MenuScreen());
 	}
 }
